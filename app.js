0867343bca2afa87c8cdbe53b9cdb29bf855db82
dkovari1 <dkovari1@gmail.com>
2022-09-30 18:24:03 +0200

    new line

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
+
+app.get("/almafa3", (res, req) => {
+    res.send("Hello")
+})
 
 app.listen(port)
