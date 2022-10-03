326e84607a5f52329bd264decf2b00d9215a01b2
markkovari <kovarimarkofficial@gmail.com>
2022-10-03 19:21:41 +0200

    chore: add environment variable checks

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+
+
 const port = process.env.PORT || 3000
+
+const envvarLoopups = [
+    'DATABASE_URL',
+    'DATABASE_PASSWORD',
+    'DATABASE_USERNAME',
+    'DATABASE_DATABASE',
+]
+
+for (const neededEnvvar of envvarLoopups) {
+    if (!process.env[neededEnvvar]) {
+        console.log(`${neededEnvvar} must be set`)
+        process.exit(1)
+    }
+}
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
